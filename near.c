c3366dcdcefd30fb8dac776e325406a2d2fc6bd1
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-03-21 16:48:27 +0530

    near.C

---
+++ near.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int round(int n)
+{
+        int a = (n / 10) * 10;
+     
+       int b = a + 10;
+ 
+     return (n - a > b - n)? b : a;
+}
+ 
+int main()
+{
+    int n = 4722;
+    cout << round(n) << endl;
+    return 0;
+}
